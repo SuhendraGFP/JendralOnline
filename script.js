cc2fea8fe708a6878a4106a3f80532aa7950a1ac
Riveria <suhendragfp21@gmail.com>
2026-06-11 10:20:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,14 +11,13 @@
    ─ Buat project di https://console.firebase.google.com/
    ─ Aktifkan: Firestore Database + Anonymous Authentication
    ================================================================ */
-const FIREBASE_CONFIG  = {
-  apiKey: "AIzaSyDmvjWxdrpjsX6KXoV6ICVySjqiEiJEuDM",
-  authDomain: "jendral-a50ff.firebaseapp.com",
-  projectId: "jendral-a50ff",
-  storageBucket: "jendral-a50ff.firebasestorage.app",
-  messagingSenderId: "159159752383",
-  appId: "1:159159752383:web:62163996db10f5ce73580a",
-  measurementId: "G-YTHVQV1L47"
+const firebaseConfig = {
+  apiKey:            "AIzaSyDEMO_REPLACE_WITH_YOUR_API_KEY",
+  authDomain:        "your-project.firebaseapp.com",
+  projectId:         "your-project-id",
+  storageBucket:     "your-project.appspot.com",
+  messagingSenderId: "000000000000",
+  appId:             "1:000000000000:web:0000000000000000"
 };
 
 /* ================================================================
@@ -131,7 +130,7 @@ function initFirebase() {
   try {
     // Hindari double-init jika modul dimuat ulang
     if (!firebase.apps.length) {
-      firebase.initializeApp(FIREBASE_CONFIG);
+      firebase.initializeApp(firebaseConfig);
     }
     db   = firebase.firestore();
     auth = firebase.auth();
